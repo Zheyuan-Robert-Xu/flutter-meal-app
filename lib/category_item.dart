424cb693deda85b109a748847e6b11250924cba2
Zheyuan -Robert XU <u6964086@anu.edu.au>
2021-12-14 23:44:29 +1100

    Screens to navigate new pages

---
+++ lib/category_item.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:meal_app/category_meals_screen.dart';
+
+class CategoryItem extends StatelessWidget {
+  final String? title;
+  final Color? color;
+
+  CategoryItem({this.title, this.color});
+
+  void selectCategory(BuildContext ctx) {
+    Navigator.of(ctx).push(MaterialPageRoute(builder: (_) {
+      return const CategoryMealsScreen();
+    })); //on the stack, MaterialPageRoute as a wrapper to handle things like the animation from the old page
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () => selectCategory(
+          context), // bind onTap to an anonymous function and now forward the context
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(15),
+      child: Container(
+        padding: const EdgeInsets.all(15),
+        child: Text(
+          title!,
+          style: Theme.of(context).textTheme.headline6,
+        ),
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [color!.withOpacity(0.7), color!],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+            borderRadius: BorderRadius.circular(15)),
+      ),
+    );
+  }
+}
